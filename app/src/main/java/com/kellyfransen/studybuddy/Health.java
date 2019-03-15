25fccf2611e0e6f02ea246c757055c17039d8dfe
marnix14 <44201873+marnix14@users.noreply.github.com>
2019-03-15 14:52:30 +0100

    in-use knoppen aanmaken

--- app/src/main/java/com/kellyfransen/studybuddy/Health.java
+++ app/src/main/java/com/kellyfransen/studybuddy/Health.java
@@ -5,20 +5,34 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Layout;
+import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class Health extends AppCompatActivity implements SensorEventListener {
-    //create buttons to count cups of coffee, water, etc
-    public int[] healthCount = new int[4];
+import java.io.Console;
+
+public class Health extends AppCompatActivity {
+    public int numButtons = 0;
+
+    ConstraintLayout healthLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_health);
+        healthLayout = findViewById(R.id.Health);
 
+        TextView addButtonTV = findViewById(R.id.addButtonTextView);
+        registerForContextMenu(addButtonTV);
+
+/*
         Button coffeeButton = (Button) findViewById(R.id.countButton);
         coffeeButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,10 +52,44 @@ public class Health extends AppCompatActivity implements SensorEventListener {
                 countTV2.setText(healthCount[1] + "");
             }
         });
+*/
     }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.healthbuttons_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        //private String name = item.getItemId();
+        Button coffeeButton = new Button(this);
+        coffeeButton.setText("Coffee");
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(400, 200);
+        layoutParams.setMargins(20, 20, 0, 0); // left, top, right, bottom
+        coffeeButton.setLayoutParams(layoutParams);
+        healthLayout.addView(coffeeButton);
+
+
+
+
+
+
+        /*
+        private String name = item.getItemId();
+        private String buttonName = name + "Button";
+
+        Button = new Button(this);
+        coffeeButton.setText("coffee");
+*/
+        return super.onContextItemSelected(item);
+    }
+
+
+}
 //    SensorManager sensorManager;
 //    TextView stepCountTV = (textView) findViewByID(R.id.stepCountTV);
 //    sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
-}
+
