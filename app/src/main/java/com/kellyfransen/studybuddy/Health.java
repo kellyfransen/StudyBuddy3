543569ab12c429dcf0cf2dbe7c685a4ec3da0589
Wijland <m.j.v.wijland@student.tue.nl>
2019-03-28 16:04:43 +0100

    button array enzo

--- app/src/main/java/com/kellyfransen/studybuddy/Health.java
+++ app/src/main/java/com/kellyfransen/studybuddy/Health.java
@@ -20,11 +20,13 @@ import android.widget.Toast;
 import java.io.Console;
 
 public class Health extends AppCompatActivity implements SensorEventListener {
-    public int numButtons = 0;
-    public int[] healthCount = new int[2];
+    public int numButtons = 3;
+    public int[] healthCount = new int[numButtons];
+    public Button[] healthButtons = new Button[numButtons];
+    public String[] healtButtonNames = new String[numButtons];
+
 
     ConstraintLayout healthLayout;
-    int buttonCount = 0;
     boolean running = false;
     SensorManager sensorManager;
     TextView stepCountTV;
@@ -39,27 +41,40 @@ public class Health extends AppCompatActivity implements SensorEventListener {
         stepCountTV = (TextView) findViewById(R.id.stepCountTV);
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
+        healthButtons[0] = (Button) findViewById(R.id.countButton);
+        healthButtons[1] = (Button) findViewById(R.id.countButton2);
+        healthButtons[2] = (Button) findViewById(R.id.countButton3);
 
-        Button coffeeButton = (Button) findViewById(R.id.countButton);
-        coffeeButton.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
-                healthCount[0]++;
-                TextView countTV0 = (TextView) findViewById(R.id.countTV);
-                countTV0.setText(healthCount[0] + "");
-            }
-        });
+        for (int i = 0; i < numButtons; i++) {
+            healthButtons[i].setOnClickListener(new View.OnClickListener() {
 
-        Button waterButton = (Button) findViewById(R.id.countButton2);
+                @Override
+                public void onClick(View v) {
+                    healthCount[i]++;
+                    healthButtons[i].setText(healthCount[i] + "");
+                }
+            });
+        }
+/*
+        final Button waterButton = (Button) findViewById(R.id.countButton2);
         waterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 healthCount[1]++;
-                TextView countTV2 = (TextView) findViewById(R.id.countTV2);
-                countTV2.setText(healthCount[1] + "");
+                waterButton.setText(healthCount[1] + "");
             }
         });
+
+        final Button pizzaButton = (Button) findViewById(R.id.countButton3);
+        pizzaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                healthCount[2]++;
+                pizzaButton.setText(healthCount[2] + "");
+            }
+        });
+        */
     }
 
     /*
@@ -121,38 +136,38 @@ public class Health extends AppCompatActivity implements SensorEventListener {
             return super.onContextItemSelected(item);
         }
     */
-        @Override
-        protected void onResume () {
-            super.onResume();
-            running = true;
-            Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
-            if (countSensor != null) {
-                sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
-            } else {
-                Toast.makeText(this, "Sensor not found!", Toast.LENGTH_SHORT).show();
-            }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        running = true;
+        Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if (countSensor != null) {
+            sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_UI);
+        } else {
+            Toast.makeText(this, "Sensor not found!", Toast.LENGTH_SHORT).show();
         }
+    }
 
-        @Override
-        protected void onPause () {
-            super.onPause();
-            running = false;
-
-        }
-
-        @Override
-        public void onSensorChanged (SensorEvent event){
-            if (running) {
-                stepCountTV.setText(String.valueOf(event.values[0]));
-            }
-
-        }
-
-
-        @Override
-        public void onAccuracyChanged (Sensor sensor,int accuracy){
-
-        }
-
+    @Override
+    protected void onPause() {
+        super.onPause();
+        running = false;
 
     }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (running) {
+            stepCountTV.setText("you have taken " + String.valueOf(event.values[0]) + " steps today!");
+        }
+
+    }
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+
+}
